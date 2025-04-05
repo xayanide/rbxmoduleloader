407c5b62b67e78b9791390d94c33801b580c20f2
xayanide <xayanide@gmail.com>
2025-04-06 05:32:14 +0800

    docs: update comment

--- ModuleContainer.lua
+++ ModuleContainer.lua
@@ -1,6 +1,6 @@
 --!strict
 --[[
-When isShared is disabled, this will remain here because it will be populated
+This will remain here because it will be populated
 with the required modules during runtime with respect to runtime context.
 After that you can require this module and use it like the global variables shared or _G
 ]]
