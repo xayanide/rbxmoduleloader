d2f253730136e39c77b901f2270d0f4a12d16672
xayanide <xayanide@gmail.com>
2025-04-06 06:22:03 +0800

    docs: update comment

--- MainModule.lua
+++ MainModule.lua
@@ -1,7 +1,7 @@
 --!strict
 --[[
 VERSION: v1.0.3
-rbxmoduleloader by xayanide (862645934) @ April 3, 2025 UTC+8
+rbxmoduleloadermodule by xayanide (862645934) @ April 3, 2025 UTC+8
 This module is meant to only have simple features with the least overhead and complexity
 ]]
 local RunService = game:GetService("RunService")
