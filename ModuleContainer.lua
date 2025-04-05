9477f4ac264e4a981f8b5623b0e8d3a1ba5142e0
xayanide <xayanide@gmail.com>
2025-04-05 23:27:58 +0800

    docs: update comment

--- ModuleContainer.lua
+++ ModuleContainer.lua
@@ -1,7 +1,7 @@
 --!strict
 --[[
-Ensure to make a ModuleScript and place this as a child of the module loader.
-Because it will be populated with the required modules during runtime with respect to runtime context.
+When isShared is disabled, this will remain here because it will be populated
+with the required modules during runtime with respect to runtime context.
 After that you can require this module and use it like the global variables shared or _G
 ]]
 return {}
