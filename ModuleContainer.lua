91afbebb29f42371e8214801b5dea2fa4f4cc357
xayanide <xayanide@gmail.com>
2025-04-05 21:27:49 +0800

    docs: update comment

--- ModuleContainer.lua
+++ ModuleContainer.lua
@@ -1,5 +1,5 @@
 --[[
-Ensure to place this as a child of the module loader.
+Ensure to make a ModuleScript and place this as a child of the module loader.
 Because it will be populated with the required modules during runtime with respect to runtime context.
 After that you can require this module and use it like the global variables shared or _G
 ]]
